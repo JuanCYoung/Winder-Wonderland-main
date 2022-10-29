7820666e400d496deebef1101ec3056bdd4d4f45
Juan <juanyoung41@gmail.com>
2022-10-29 13:55:26 +0700

    Add

--- Assets/Script/Lever/LevelSystem.cs
+++ Assets/Script/Lever/LevelSystem.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LevelSystem : MonoBehaviour
 {   
     private Rigidbody2D treeRb;
     public float dropforce = 5;
 
-    void Start()
-    {
-        treeRb = GetComponent<Rigidbody2D>();
-        treeRb.AddForce(Vector2.down * dropforce, ForceMode2D.Impulse);
-        treeRb.gravityScale = 1;        
-    }
-
+    public bool dropNo ;
+    public UnityEvent interactionAction;
+    
     public void ItsFalling(){
         treeRb = GetComponent<Rigidbody2D>();
         treeRb.AddForce(Vector2.down * dropforce, ForceMode2D.Impulse);
         treeRb.gravityScale = 1;  
     }
 
+    public void makeTrue(){
+        dropNo = true;
+    }
+
     private void Update() {
-        
+        if(dropNo){
+            ItsFalling();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Penahan"))
+        {
+            treeRb.bodyType = RigidbodyType2D.Static;
+        }
     }
 
     
